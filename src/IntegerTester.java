4bb719dd220f8fa02c6dd552ffeff71fef04bb0c
davidlee <david20@seas.upenn.edu>
2024-10-04 20:53:37 -0400

    1st commit

---
+++ src/IntegerTester.java
@@ -0,0 +1,6 @@
+public class IntegerTester {
+    public static void main(String[] args) {
+        PositiveInteger pi = new PositiveInteger(6);
+        System.out.println(pi.isPerfect());
+    }
+}
--- src/PositiveInteger.java
+++ src/PositiveInteger.java
@@ -1,2 +1,33 @@
 public class PositiveInteger {
+
+    private int num;
+
+    public PositiveInteger(int number){
+        num = number;
+    }
+
+    public boolean isPerfect() {
+        int sum = 0;
+
+        for (int i = 1; i <num; i++){
+            if (num % i ==0){
+                sum =sum+i;
+            }
+
+        }
+
+
+        return sum == num;
+    }
+
+
+    public boolean isAbundant() {
+        return false;
+    }
+
+    public boolean isNarcissistic() {
+        return false;
+    }
+
 }
+
